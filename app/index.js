8c36996f3bb0037631c03912285a633d47fa1c22
chmeshra <chmeshra@visa.com>
2016-03-18 11:49:26 -0700

    Adding a PromptContainer and PlayerOne view

--- app/index.js
+++ app/index.js
@@ -1,16 +1,5 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-//var routes = require('./config/routes');
+var routes = require('./config/routes');
 
-
-
-
-//ReactDOM.render(routes, document.getElementById('app'));
-
-function HelloWorld(props) {
-  return (
-    <div> Hello World {props.name} </div>
-  )
-}
-
-ReactDOM.render(<HelloWorld name='chetan' />, document.getElementById('app'));
+ReactDOM.render(routes, document.getElementById('app'));
