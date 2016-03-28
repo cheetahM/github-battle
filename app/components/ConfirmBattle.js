f627fc1757d753e9c4be8f290158c5ea422bb770
chmeshra <chmeshra@visa.com>
2016-03-27 18:39:56 -0700

    Adding a User wrapper component

--- app/components/ConfirmBattle.js
+++ app/components/ConfirmBattle.js
@@ -1,37 +1,27 @@
 var React = require('react');
 var PropTypes = React.PropTypes;
 var styles = require('../styles');
-var Link = require('react-router').Link;
+var ReactRouter = require('react-router');
+var Link = ReactRouter.Link;
+var UserDetailsWrapper = require('./UserDetailsWrapper');
 var UserDetails = require('./UserDetails');
-
-function puke (object) {
-  return <pre>{JSON.stringify(object , null, ' ')}</pre>
-}
 
 function ConfirmBattle (props) {
   return props.isLoading === true
-    ? <p> Loading! </p>
+    ? <p>LOADING</p>
     : <div className="jumbotron col-sm-12 text-center" style={styles.transparentBg}>
         <h1>Confirm Players</h1>
         <div className='col-sm-8 col-sm-offset-2'>
-          <div className='col-sm-6'>
-            <p className='lead'>
-              Player 1
-            </p>
+          <UserDetailsWrapper header='Player 1'>
             <UserDetails info={props.playersInfo[0]} />
-          </div>
-          <div className='col-sm-6'>
-            <p className='lead'>
-              Player 2
-            </p>
+          </UserDetailsWrapper>
+          <UserDetailsWrapper header='Player 2'>
             <UserDetails info={props.playersInfo[1]} />
-          </div>
+          </UserDetailsWrapper>
         </div>
         <div className='col-sm-8 col-sm-offset-2'>
           <div className='col-sm-12' style={styles.space}>
-            <button type='button' className='btn btn-lg btn-success' onClick={props.onInitiateBattle}>
-              Initiate Battle
-            </button>
+            <button type='button' className='btn btn-lg btn-success' onClick={props.onInitiateBattle}>Initiate Battle!</button>
           </div>
           <div className='col-sm-12' style={styles.space}>
             <Link to='/playerOne'>
@@ -45,7 +35,7 @@ function ConfirmBattle (props) {
 ConfirmBattle.propTypes = {
   isLoading: PropTypes.bool.isRequired,
   onInitiateBattle: PropTypes.func.isRequired,
-  playersInfo: PropTypes.array.isRequired
+  playersInfo: PropTypes.array.isRequired,
 }
 
 module.exports = ConfirmBattle;
