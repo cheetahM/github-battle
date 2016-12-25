74c134f55b7346709891ed12be8138064ce45273
Chetan Meshram <chmeshra@visa.com>
2016-12-25 15:31:41 -0800

    adding private stateless component for StartOver and a MainContainer

--- app/components/Results.js
+++ app/components/Results.js
@@ -1,15 +1,52 @@
 var React = require('react');
 var PropTypes = React.PropTypes;
+var styles = require('../styles');
+var UserDetails = require('../components/UserDetails');
+var UserDetailsWrapper = require('../components/UserDetailsWrapper');
+var Link = require('react-router').Link;
+var MainContainer = require('./MainContainer');
 
-function puke(obj) {
-  return <pre>{JSON.stringify(obj, 2,' ' )}</pre>
+function StartOver() {
+  return(
+    <div className='col-sm-12' style={styles.space}>
+      <Link to='/playerOne'>
+        <button type='button' className='btn btn-lg btn-danger'>Start Over</button>
+      </Link>
+    </div>
+  );
 }
 
 function Results(props) {
+  if(props.isLoading === true) {
     return (
-      <div>Results: {puke(props)}</div>
-    );
-};
+      <p> Loading...</p>
+    )
+  }
+  if(props.scores[0] === props.scores[1]) {
+    return (
+      <MainContainer>
+        <h1>Results: Its a Tie</h1>
+        <StartOver />
+      </MainContainer>
+    )
+  }
+  var winningIndex = props.scores[0] > props.scores[1]? 0 : 1;
+  var losingIndex = winningIndex === 0? 1 : 0;
+  return (
+    <div>
+      <MainContainer>
+        <h1>Results</h1>
+        <UserDetailsWrapper header='Winner'>
+          <UserDetails score={props.scores[winningIndex]} info={props.playersInfo[winningIndex]} />
+        </UserDetailsWrapper>
+        <UserDetailsWrapper header='Loser'>
+          <UserDetails score={props.scores[losingIndex]} info={props.playersInfo[losingIndex]} />
+        </UserDetailsWrapper>
+      </MainContainer>
+      <StartOver />
+    </div>
+  );
+}
 
 Results.propTypes = {
   isLoading: PropTypes.bool.isRequired,
--- app/components/UserDetails.js
+++ app/components/UserDetails.js
@@ -2,9 +2,6 @@ var React = require('react');
 var PropTypes = React.PropTypes;
 
 function UserDetails (user) {
-  console.log(user);
-  console.log(user.score);
-  console.log(user.info);
   return (
     <div>
       {!!user.score && <li className="list-group-item"><h3>Score: {user.score}</h3></li>}
